7c05699fb7501f58de69cf56d9130646bb0422c6
Gerard Hynes <gkhynes@gmail.com>
2018-11-18 18:24:00 +0000

    Add event delegation

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const current = document.querySelector("#current");
-const imgs = document.querySelectorAll(".imgs img");
+const imgs = document.querySelector(".imgs");
+const img = document.querySelectorAll(".imgs img");
 const opacity = 0.6;
 
 // Set first img opacity
-imgs[0].style.opacity = opacity;
+img[0].style.opacity = opacity;
 
-imgs.forEach(img => img.addEventListener("click", imgClick));
+imgs.addEventListener("click", imgClick);
 
 function imgClick(e) {
   // Reset opacity
-  imgs.forEach(img => (img.style.opacity = 1));
+  img.forEach(img => (img.style.opacity = 1));
 
   // Change current img to clicked img
   current.src = e.target.src;
